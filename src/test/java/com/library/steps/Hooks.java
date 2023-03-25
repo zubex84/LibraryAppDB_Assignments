51067a6686443def94f62f1dd5dc9528b00549d4
zubex84 <marko_zubex@hotmail.com>
2023-03-25 16:29:25 -0400

    Added missing tags for DB tests

--- src/test/java/com/library/steps/Hooks.java
+++ src/test/java/com/library/steps/Hooks.java
@@ -13,7 +13,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Hooks {
 
-    @Before("ui")
+    @Before("@ui")
     public void setUp(){
         System.out.println("this is coming from BEFORE");
         Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -23,7 +23,7 @@ public class Hooks {
 
     }
 
-    @After
+    @After("@ui")
     public void tearDown(Scenario scenario){
         System.out.println("this is coming from AFTER");
 
@@ -37,14 +37,16 @@ public class Hooks {
     }
 
 
-    @Before
+    @Before("@db")
     public void setUpDB(){
         DB_Util.createConnection();
+        System.out.println("DB connection is DONE");
     }
 
-    @After
+    @After("@db")
     public void destroyDB(){
         DB_Util.destroy();
+        System.out.println("DB connection is closed");
     }
 
 
