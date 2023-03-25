83f664c4fe4ec3831969ac0b7c768080e6c5cf8c
zubex84 <marko_zubex@hotmail.com>
2023-03-25 15:26:44 -0400

    Added UserInfoStepDef

---
+++ src/test/java/com/library/steps/UserInfoStepDef.java
@@ -0,0 +1,26 @@
+package com.library.steps;
+
+import io.cucumber.java.en.*;
+
+public class UserInfoStepDef {
+
+    @Given("Establish the database connection")
+    public void establish_the_database_connection() {
+
+    }
+
+
+    @When("Execute query to get all IDs from users")
+    public void execute_query_to_get_all_i_ds_from_users() {
+
+    }
+
+
+    @Then("verify all users has unique ID")
+    public void verify_all_users_has_unique_id() {
+
+    }
+
+
+
+}
---
+++ src/test/resources/library.sql
@@ -0,0 +1,8 @@
+select count(id) from users;
+-- 1855
+
+
+select distinct count(id) from users;
+-- 1855
+
+-- result -> pass manually
