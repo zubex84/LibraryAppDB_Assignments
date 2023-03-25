451c48c997ceade78b84224bced389dd39a53ca4
zubex84 <marko_zubex@hotmail.com>
2023-03-25 15:37:10 -0400

    Added test db@

--- src/test/java/com/library/steps/UserInfoStepDef.java
+++ src/test/java/com/library/steps/UserInfoStepDef.java
@@ -1,23 +1,46 @@
 package com.library.steps;
 
+import com.library.utility.DB_Util;
 import io.cucumber.java.en.*;
+import org.junit.Assert;
 
 public class UserInfoStepDef {
 
+    String actualUserCount;
+
     @Given("Establish the database connection")
     public void establish_the_database_connection() {
-
+        DB_Util.createConnection();
     }
 
 
     @When("Execute query to get all IDs from users")
     public void execute_query_to_get_all_i_ds_from_users() {
+        String query = "select count(id) from users";
+        DB_Util.runQuery(query);
+
+        actualUserCount = DB_Util.getFirstRowFirstColumn();
+        System.out.println("actualUserCount = " + actualUserCount);
+
 
     }
 
 
     @Then("verify all users has unique ID")
     public void verify_all_users_has_unique_id() {
+
+        String query = "select distinct count(id) from users";
+        DB_Util.runQuery(query);
+
+        String expectedUserCount = DB_Util.getFirstRowFirstColumn();
+        System.out.println("expectedUserCount = " + expectedUserCount);
+
+        // make assertions
+        Assert.assertEquals(expectedUserCount, actualUserCount );
+
+
+        // close connection
+        DB_Util.destroy();
 
     }
 
